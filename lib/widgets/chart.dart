c85f80e019e06f0adfbf316cb974d246f856f058
mubinbin <dinobunmu@gmail.com>
2022-02-25 22:55:47 -0600

    corrected some typos

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -7,27 +7,27 @@ class Chart extends StatelessWidget {
 
   const Chart({Key? key, required this.recentTransactions}) : super(key: key);
 
-  List<Map<String, Object>> get groupedTranscations =>
+  List<Map<String, Object>> get groupedTransactions =>
       List.generate(7, (index) {
         final weekDay = DateTime.now().subtract(Duration(days: index));
-        double spandTotal = 0.0;
+        double spendTotal = 0.0;
 
         for (int i = 0; i < recentTransactions!.length; i++) {
           if (recentTransactions![i].date.day == weekDay.day &&
               recentTransactions![i].date.month == weekDay.month &&
               recentTransactions![i].date.year == weekDay.year &&
               recentTransactions != null) {
-            spandTotal += recentTransactions![i].amount;
+            spendTotal += recentTransactions![i].amount;
           }
         }
 
-        return {"day": DateFormat.E().format(weekDay), "amount": spandTotal};
+        return {"day": DateFormat.E().format(weekDay), "amount": spendTotal};
       });
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: groupedTranscations
+      children: groupedTransactions
           .map(
             (m) => Text(
                 '${m['day']}: ${(m['amount'] as double) * 100.truncateToDouble() / 100}'),
