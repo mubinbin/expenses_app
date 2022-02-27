ee4fedf3e251d344ebab838f454c7a17f34b80ac
Binbin Mu <dinobunmu@gmail.com>
2022-02-26 22:01:52 -0600

    added variable types

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -4,15 +4,16 @@ import './text_input.dart';
 class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
 
-  const NewTransaction({Key? key, required this.addNewTransaction}) : super(key: key);
+  const NewTransaction({Key? key, required this.addNewTransaction})
+      : super(key: key);
 
   @override
   State<NewTransaction> createState() => _NewTransactionState();
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
